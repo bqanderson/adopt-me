c8aa228583e00237ba471385337415c115429877
Brian Q Anderson <bqanderson@me.com>
2020-02-11 13:32:23 -0600

    add npm scripts, eslint, Prettier, gitignore

--- src/App.js
+++ src/App.js
@@ -7,28 +7,24 @@ const Pet = ({ name, animal, breed }) => {
 }
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: 'Fiona',
-        animal: 'Dog',
-        breed: 'Apple head chihuahua'
-      }),
-      React.createElement(Pet, {
-        name: 'Sammy',
-        animal: 'Dog',
-        breed: 'Dear head chihuahua'
-      }),
-      React.createElement(Pet, {
-        name: 'Dionk',
-        animal: 'Cat',
-        breed: 'Mixed'
-      })
-    ]
-  );
-};
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Fiona',
+      animal: 'Dog',
+      breed: 'Apple head chihuahua',
+    }),
+    React.createElement(Pet, {
+      name: 'Sammy',
+      animal: 'Dog',
+      breed: 'Dear head chihuahua',
+    }),
+    React.createElement(Pet, {
+      name: 'Dionk',
+      animal: 'Cat',
+      breed: 'Mixed',
+    }),
+  ])
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
