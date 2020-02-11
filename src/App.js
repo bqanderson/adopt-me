4fde515ea20e0737ad765e751a4ec81af7b0e741
Brian Q Anderson <bqanderson@me.com>
2020-02-11 08:23:34 -0600

    Initial commit. Write first React code, no JSX.

---
+++ src/App.js
@@ -0,0 +1,34 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed),
+  ])
+}
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: 'Fiona',
+        animal: 'Dog',
+        breed: 'Apple head chihuahua'
+      }),
+      React.createElement(Pet, {
+        name: 'Sammy',
+        animal: 'Dog',
+        breed: 'Dear head chihuahua'
+      }),
+      React.createElement(Pet, {
+        name: 'Dionk',
+        animal: 'Cat',
+        breed: 'Mixed'
+      })
+    ]
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
